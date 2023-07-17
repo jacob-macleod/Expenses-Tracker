2f5898baa51cfb3b195fababdc485e0dfeb8f891
jacob-macleod <jacob.e.macleod@gmail.com>
2023-07-17 10:42:13 +0100

    Added some predefined expenses

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expenses_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,6 +11,21 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      amount: 10.99, 
+      title: "Groceries", 
+      date: DateTime.now(), 
+      category: Category.food,
+    ),
+    Expense(
+      title: "New shoes", 
+      amount: 99.99, 
+      date: DateTime.now(), 
+      category: Category.leisure,
+    ),
+  ]
+
   @override
   Widget build(context) {
     return const Scaffold(
